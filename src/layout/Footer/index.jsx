d0cd25b1c16f50ace5880b0898fd6bf1dba34532
mrwdhasd0903 <2553378438@qq.com>
2021-07-05 22:06:44 +0800

    完成页脚

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -1,7 +1,8 @@
-import { useState } from "myact";
+import { useState, Svg } from "myact";
+import { Link } from "react-router-dom";
 import "./index.scss";
 export default function Index() {
-  const [bg] = useState("/image/banner-bg2.jpg");
+  const [bg] = useState("/image/footer-bg.jpg");
   return (
     <div className="Footer">
       <div
@@ -11,7 +12,50 @@ export default function Index() {
         }}
       ></div>
       <div className="modal"></div>
-      <div className="front"></div>
+      <div className="front">
+        <div className="boxs">
+          <div className="box qrcode">
+            {/* eslint-disable-next-line jsx-a11y/alt-text */}
+            <img src="https://wdhhh.cn/upload/io/img/2020/4/6/1586161319792.jpg" />
+          </div>
+          <div className="box links">
+            <div className="title">友情链接</div>
+            <Link className="link" to="#">
+              友链空位,欢迎互联
+            </Link>
+            <Link className="link" to="#">
+              友链空位,欢迎互联
+            </Link>
+            <Link className="link" to="#">
+              友链空位,欢迎互联
+            </Link>
+            <Link className="link" to="#">
+              查看更多&申请 >>
+            </Link>
+          </div>
+          <div className="box contact">
+            <div className="title">联系小王</div>
+            <div className="info">
+              <Svg name="qq" />
+              <span>2553378438</span>
+            </div>
+            <div className="info">
+              <Svg name="wechat" />
+              <span>codemeow</span>
+            </div>
+            <Link className="github">
+              <Svg name="github" />
+            </Link>
+          </div>
+          <div className="box">
+            唯有时间对我们一视同仁
+          </div>
+        </div>
+        <div className="copyright">
+          粤ICP备20003149号 | Copyright © 2019 - 2022 - www.wdhhh.cn | By
+          小王的程序人生
+        </div>
+      </div>
     </div>
   );
 }
