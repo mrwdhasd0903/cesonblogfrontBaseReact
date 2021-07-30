1913e5faf15ded2502e89085ae4ee26380ca6f35
mrwdhasd0903 <2553378438@qq.com>
2021-07-30 23:01:57 +0800

    初始化tag页面

---
+++ src/pages/tag/components/Tags/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./index.scss";
+import data from "./data";
+
+export default function index() {
+  return <div></div>;
+}
--- src/pages/tag/index.jsx
+++ src/pages/tag/index.jsx
@@ -1,4 +1,5 @@
 import React from "myact";
+import "./index.scss";
 
 export default function Index() {
   return <div>标签</div>;
